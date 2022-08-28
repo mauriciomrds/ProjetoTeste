2682d9976b301dfc3bdbfe4bf65431492063feb7
lenovo-20 <mauricio_reisdasilva@hotmail.com>
2022-08-28 20:28:58 -0300

    Criando projeto no git new

---
+++ ProjetoTeste/src/projetojavatest/ProjetoTeste.java
@@ -0,0 +1,29 @@
+
+package projetojavatest;
+import java.util.Scanner;
+public class ProjetoTeste {
+
+	public static void main(String[]args) {
+	System.out.println("=================== SISTEMA DE LOGIN ==========");
+	@SuppressWarnings("resource")
+	Scanner sc = new Scanner(System.in);
+	
+	String login,senha;
+	
+	System.out.print("Digite o seu Login: ");
+	login=sc.nextLine();
+	
+	System.out.print("Digite a sua Senha: ");
+	senha=sc.nextLine();
+	//Teste//
+	if(login.equals("Mauricio")&& senha.equals("405060")) {
+		System.out.println("Usuário e senha correta");
+	}else {
+		System.out.println("Usuário ou senha incorreta");
+			}
+	    System.out.println("FIM ====>");
+	    System.out.println();
+	
+	    
+	}
+}
--- projetojava/src/projetojava/ProjetoTeste.java
+++ projetojava/src/projetojava/ProjetoTeste.java
@@ -14,12 +14,15 @@ public class ProjetoTeste {
 	
 	System.out.print("Digite a sua Senha: ");
 	senha=sc.nextLine();
-	
+	//Teste//
 	if(login.equals("Mauricio")&& senha.equals("405060")) {
 		System.out.println("Usuário e senha correta");
 	}else {
 		System.out.println("Usuário ou senha incorreta");
 			}
 	    System.out.println("FIM ====>");
+	    System.out.println();
+	
+	    
 	}
 }
